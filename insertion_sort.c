0f849aba13f18dcd6d8ae9a91ac9d7984929a8f4
John Caruthers <john.d.caruthers@gmail.com>
2020-03-24 21:12:20 -0400

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int insert(int array[]);
+
 int main(void)
 {
+    //Aquire an array of numbers from the user
 
     int max_numb = get_int("Max numbers in array: ");
 
@@ -14,10 +17,43 @@ int main(void)
         numb_array[i] = n;
     }
 
+
+    printf("Array before sorting: \n");
     for (int i = 0; i < max_numb; i++)
     {
         printf("%i ", numb_array[i]);
     }
 
     printf("\n");
+
+    /////////////Insertion Sort Algorithm//////////////
+
+    for (int j = 1; j < max_numb; j++)
+    {
+        int k = max_numb;
+
+        while (k > 0)
+        {
+            int l = max_numb - k;
+
+            if (numb_array[j] < numb_array[l])
+            {
+                /////SWAP///
+                int i = numb_array[j];
+                numb_array[j] = numb_array[l];
+                numb_array[l] = i;
+            }
+            k--;
+        }
+    }
+
+    ///print array again to see if in order
+    printf("\n\nArray after sorting:\n");
+     for (int i = 0; i < max_numb; i++)
+    {
+        printf("%i ", numb_array[i]);
+    }
+
+    printf("\n");
+
 }
